692fc80eacc4dad4781a76b3b78fab5597fc221f
Daniel Gih <gih.daniel@gmail.com>
2016-06-16 20:29:52 -0700

    sec4, lec42

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { title:'JS: The Good Parts' },
+    { title:'Harry Potter' },
+    { title:'The Martian' },
+    { title:'Twilight' },
+    { title:'Everybody Eats' },
+  ]
+}
